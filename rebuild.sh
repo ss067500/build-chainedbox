4b8e167f01c0f678471f9ed5f30d8d3b8f35ba01
WingonWu <wingonwu@outlook.com.com>
2022-03-07 12:00:25 +0800

    no message

--- rebuild.sh
+++ rebuild.sh
@@ -9,6 +9,7 @@ DTB=dtbs
 IDB=loader/idbloader.bin
 UBOOT=loader/uboot.img
 TRUST=loader/trust.bin
+UBOOT_WITH_FIP=/loader/btld-rk3328.bin
 echo 当前工作目录："$WORK_DIR"
 echo -e "01.01 读取镜像"
 #设置镜像路径
@@ -47,75 +48,14 @@ echo -e "完成"
 echo -e "03.01 复制文件"
 echo "复制文件"
 cp -v ${WORK_DIR}/$DTB/*.dtb $mount_point/boot/
-cp -v ${WORK_DIR}/l1pro/install-docker.sh $mount_point/root/
-cp -v ${WORK_DIR}/l1pro/install-omv.sh $mount_point/root/
-cp -v ${WORK_DIR}/l1pro/pwm-fan.service $mount_point/etc/systemd/system/
-cp -v ${WORK_DIR}/l1pro/pwm-fan.pl $mount_point/usr/bin/ && chmod 700 $mount_point/usr/bin/pwm-fan.pl
+#cp -v ${WORK_DIR}/l1pro/install-docker.sh $mount_point/root/
+#cp -v ${WORK_DIR}/l1pro/install-omv.sh $mount_point/root/
+#cp -v ${WORK_DIR}/l1pro/pwm-fan.service $mount_point/etc/systemd/system/
+#cp -v ${WORK_DIR}/l1pro/pwm-fan.pl $mount_point/usr/bin/ && chmod 700 $mount_point/usr/bin/pwm-fan.pl
 
 echo -e "完成"
 
-echo -e "04.01 修改引导分区相关配置"
 
-
-echo "修改引导分区相关配置 ... "
-cd ${WORK_DIR}
-
-sed -i '/^verbosity/cverbosity=7' $mount_point/boot/armbianEnv.txt && \
-sed -i '/rootfstype=ext4/a rootflags=rw' $mount_point/boot/armbianEnv.txt && \
-echo "extraargs=usbcore.autosuspend=-1" >> $mount_point/boot/armbianEnv.txt && \
-echo "extraboardargs=" >> $mount_point/boot/armbianEnv.txt && \
-echo "fdtfile=rk3328-l1pro-1296mhz.dtb" >> $mount_point/boot/armbianEnv.txt && \
-echo "usbstoragequirks=0x05e3:0x0612:u,0x1d6b:0x0003:u,0x05e3:0x0610:u" >> $mount_point/boot/armbianEnv.txt && \
-sed -i 's/0x9000000/0x39000000/' $mount_point/boot/boot.cmd && \
-sed -i 's#${prefix}dtb/${fdtfile}#${prefix}/${fdtfile}#' $mount_point/boot/boot.cmd
-mkimage -C none -T script -d $mount_point/boot/boot.cmd $mount_point/boot/boot.scr
-
-# patch rootfs
-echo "修改为国内软件源"
-cat > $mount_point/etc/apt/sources.list <<EOF
-deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
-deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
-deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
-deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
-EOF
-
-cat > /etc/apt/sources.list.d/armbian.list <<EOF
-deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/armbian/ bullseye main bullseye-utils bullseye-desktop
-EOF
-
-sed -i 's/ENABLED=true/#ENABLED=true/' $mount_point/etc/default/armbian-zram-config
-sed -i 's/ENABLED=true/#ENABLED=true/' $mount_point/etc/default/armbian-ramlog
-rm -f $mount_point/etc/systemd/system/getty.target.wants/serial-getty\@ttyS2.service
-ln -sf /usr/share/zoneinfo/Asia/Shanghai $mount_point/etc/localtime
-sed -i 's/Rock 64/Chainedbox/' $mount_point/etc/armbian-image-release
-sed -i 's/rock64/Chainedbox/' $mount_point/etc/armbian-image-release
-sed -i 's/rock64/Chainedbox/' $mount_point/etc/armbian-release
-sed -i 's/Rock 64/Chainedbox/' $mount_point/etc/armbian-release
-sed -i 's/rock64/Chainedbox/' $mount_point/etc/hostname
-sed -i 's/rock64/Chainedbox/' $mount_point/etc/host
-
-sync
-
-echo "进入 CHROOT 模式更新系统组件"
-
-chroot $mount_point <<EOF
-su
-
-echo "启动风扇服务"
-systemctl enable pwm-fan.service
-
-echo "锁定内核文件，防止升级的时候 我家云 的专用内核被通用内核替换导致不开机"
-apt-mark hold linux-dtb-legacy-rockchip64 linux-image-legacy-rockchip64 linux-dtb-current-rockchip64 linux-image-current-rockchip64 linux-dtb-edge-rockchip64 linux-image-edge-rockchip64
-
-echo "取消休眠"
-sudo systemctl mask sleep.targetsuspend.target hibernate.target hybrid-sleep.target sleep.target suspend.target
-
-echo "修改时区为东八区"
-ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
-	dpkg-reconfigure --frontend noninteractive tzdata
-echo "完成 退出 CHROOT 模式"
-exit
-EOF
 sync
 
 cd ${WORK_DIR}
@@ -124,9 +64,19 @@ umount -f $mount_point
 
 echo "添加引导项： idb,uboot,trust"
 
-dd if=${IDB} of=${imgfile} seek=64 bs=512 conv=notrunc status=none && echo "idb patched: ${IDB}" 成功 || { echo "idb patch 失败"; exit 1; }
-dd if=${UBOOT} of=${imgfile} seek=16384 bs=512 conv=notrunc status=none && echo "uboot patched: ${UBOOT}" 成功 || { echo "u-boot patch 失败"; exit 1; }
-dd if=${TRUST} of=${imgfile} seek=24576 bs=512 conv=notrunc status=none && echo "trust patched: ${TRUST}" 成功 || { echo "trust patch 失败"; exit 1; }
+#dd if=${IDB} of=${imgfile} seek=64 bs=512 conv=notrunc status=none && echo "idb patched: ${IDB}" 成功 || { echo "idb patch 失败"; exit 1; }
+#dd if=${UBOOT} of=${imgfile} seek=16384 bs=512 conv=notrunc status=none && echo "uboot patched: ${UBOOT}" 成功 || { echo "u-boot patch 失败"; exit 1; }
+#dd if=${TRUST} of=${imgfile} seek=24576 bs=512 conv=notrunc status=none && echo "trust patched: ${TRUST}" 成功 || { echo "trust patch 失败"; exit 1; }
+
+echo "写入 bootloader ..."
+echo "dd if=${UBOOT_WITH_FIP}  of=${imgfile} conv=fsync,notrunc bs=512 skip=1 seek=1"
+echo "dd if=${UBOOT_WITH_FIP}  of=${imgfile} conv=fsync,notrunc bs=1 count=444"
+
+dd if=${UBOOT_WITH_FIP}  of=${imgfile} conv=fsync,notrunc bs=512 skip=1 seek=1
+dd if=${UBOOT_WITH_FIP}  of=${imgfile} conv=fsync,notrunc bs=1 count=444
+
+sync
+echo "完成"
 
 rm -rf $imgdir/*.sha
 rm -rf $imgdir/*.txt
---
+++ userpatches/customize-image.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#对镜像进行一些定制操作
+wget -P /boot https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/dtbs/rk3328-l1pro-1512mhz.dtb
+wget -P /root https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/install-docker.sh
+wget -P /root https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/install-omv.sh
+wget -P /root https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/install-zerotier.sh
+wget -P /root https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/install-cups.sh
+#下载风扇服务
+wget -P /etc/systemd/system https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/pwm-fan.service
+wget -P /usr/bin https://github.com/WingonWu/Chainedbox-build/blob/b7ac13bbc48609f4e3b24568cdd54c507027868e/l1pro/pwm-fan.pl && chmod 700 /usr/bin/pwm-fan.pl
+#启动风扇服务
+systemctl enable pwm-fan.service
+
+#锁定内核文件，防止升级的时候 我家云 的专用内核被通用内核替换导致不开机 
+apt-mark hold linux-dtb-legacy-rockchip64 linux-image-legacy-rockchip64 linux-dtb-current-rockchip64 linux-image-current-rockchip64 linux-dtb-edge-rockchip64 linux-image-edge-rockchip64
+
+#取消休眠
+sudo systemctl mask sleep.targetsuspend.target hibernate.target hybrid-sleep.target sleep.target suspend.target
+
+sed -i 's/ENABLED=true/#ENABLED=true/' /etc/default/armbian-zram-config
+sed -i 's/ENABLED=true/#ENABLED=true/' /etc/default/armbian-ramlog
+rm -f /etc/systemd/system/getty.target.wants/serial-getty\@ttyS2.service
+
+#设置默认密码
+echo root:1234 | chpasswd
+#删除登录修改密码的选项
+rm -f /root/.not_logged_in_yet
+#删除时区
+rm -rf /etc/localtime
+#修改时区
+echo "修改时区为东八区"
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime && \
+	dpkg-reconfigure --frontend noninteractive tzdata
+echo Etc/UTC > /etc/timezone
+
+# 修改 Hostname
+sed -i 's/Rock 64/Chainedbox/' /etc/armbian-image-release
+sed -i 's/rock64/Chainedbox/' /etc/armbian-image-release
+sed -i 's/rock64/Chainedbox/' /etc/armbian-release
+sed -i 's/Rock 64/Chainedbox/' /etc/armbian-release
+sed -i 's/rock64/Chainedbox/' /etc/hostname
+sed -i 's/rock64/Chainedbox/' /etc/host
+
+#修改镜像源
+echo "修改为国内软件源"
+cat > /etc/apt/sources.list <<- EOF
+deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye main contrib non-free
+deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-updates main contrib non-free
+deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian/ bullseye-backports main contrib non-free
+deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/debian-security bullseye-security main contrib non-free
+EOF
+
+cat > /etc/apt/sources.list.d/armbian.list <<- EOF
+deb [arch=arm64,armhf] https://mirrors.tuna.tsinghua.edu.cn/armbian/ bullseye main bullseye-utils bullseye-desktop
+EOF
+
+#更新源
+apt-get update&&apt-get -y upgrade
+
+#清除安装包
+apt-get clean
+
+#关闭自动休眠,长时间不登录导致无法连上ssh,不知道是否有效，有待观察
+systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target sleep.targetsuspend.target
+
